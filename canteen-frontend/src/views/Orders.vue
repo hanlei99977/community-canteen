ad721d6adab349a96a47dc5334fc6f30a832138a
HanLei <1321617737@qq.com>
2026-04-02 18:39:48 +0800

    前端-优化订单页面

--- canteen-frontend/src/views/Orders.vue
+++ canteen-frontend/src/views/Orders.vue
@@ -9,17 +9,17 @@
       <el-table-column type="expand">
         <template #default="scope">
 
-          <el-table :data="scope.row.items" border>
-            <el-table-column prop="dish_name" label="菜品" />
-            <el-table-column prop="quantity" label="数量" />
-            <el-table-column prop="price" label="价格" />
+          <el-table :data="scope.row.items" border align="center">
+            <el-table-column prop="dish_name" label="菜品" align="center" />
+            <el-table-column prop="price" label="价格" align="center" />
+            <el-table-column prop="quantity" label="数量" align="center" />
           </el-table>
 
         </template>
       </el-table-column>
 
       <el-table-column prop="order_id" label="订单ID" />
-      <el-table-column prop="order_for_user_id" label="为谁购买" />
+      <el-table-column prop="order_for_user_name" label="为谁购买" />
       <el-table-column prop="canteen_name" label="食堂" />
       <el-table-column prop="total_price" label="总价" />
       <el-table-column prop="create_time" label="时间" />
